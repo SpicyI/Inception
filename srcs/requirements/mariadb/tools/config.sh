565cb8b6b4f20ca8a272342df010deb804424b2c
spicyi <elkhayyate.driss@outlook.com>
2023-09-04 15:44:28 -0500

    maridb fixed

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -2,7 +2,13 @@
 
 service mariadb start
 
+echo  "=> Waiting for confirmation of MariaDB service startup"
+
 sleep 1
+
+echo "=> Creating MariaDB admin user with ${MYSQL_ROOT_PASSWORD} password"
+
+# CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 mysql -sfu root  <<EOS
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; 
@@ -11,10 +17,16 @@ DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE $MYSQL_DATABASE;
-CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 EOS
 
-mysqladmin -u root -p'$MYSQL_ROOT_PASSWORD' shutdown
-mysqld --bind 0.0.0.0
+
+echo "=> Done!"
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+echo "=> restarting MariaDB ..."
+# echo "=> Stopping MariaDB ..."
+mysqld # --bind 0.0.0.0
+
+echo "=> fail!"
+# echo "=> MariaDB is ready to use."
