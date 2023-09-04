28f16158ec27d09a126a091b6561aa27071e8a02
SpicyI <driss.elkhayyate@outlook.com>
2023-09-04 15:43:16 +0100

    wordpress and nginx test

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-mysql -sfu root <<EOS
--- set root password
-UPDATE mysql.user SET Password=PASSWORD('Elkhayyate@mariadb') WHERE User='root';
--- delete anonymous users
+service mariadb start
+
+sleep 1
+
+mysql -sfu root  <<EOS
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'Elkhayyate@rootdb'; 
 DELETE FROM mysql.user WHERE User='';
--- delete remote root capabilities
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
--- drop database 'test'
 DROP DATABASE IF EXISTS test;
--- also make sure there are lingering permissions to it
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
--- make changes immediately
+CREATE DATABASE wordpress;
+CREATE USER 'GAME_MASTER'@'localhost' IDENTIFIED BY 'TETRIS@MDB';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'GAME_MASTER'@'localhost';
 FLUSH PRIVILEGES;
 EOS
+
+mysqladmin -u root -p'Elkhayyate@rootdb' shutdown
+mysqld --bind 0.0.0.0
--- srcs/requirements/wordpress/tools/WP_entrypoint.sh
+++ srcs/requirements/wordpress/tools/WP_entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-wp core download
+# copy the template config file
+sleep 10
+cd /var/www/wordpress
+cp wp-config-sample.php wp-config.php
 
+# manually replacing each field with the right credential
+
+sed -i "s/username_here/$WORDPRESS_DB_USER/g" wp-config.php
+sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/g" wp-config.php
+sed -i "s/localhost/$WORDPRESS_DB_HOST/g" wp-config.php
+sed -i "s/database_name_here/wordpress/g" wp-config.php
+
+wp --allow-root core install --url="del-kahy.42.fr" --title="landing page" --admin_user=delkhayADM --admin_password="Ekhayyate@wp" --admin_email="del-khay@student.1337.ma"
+
+
+# ! add user (default)
+
+echo "starting fpm ... "
+php-fpm7.4 -F
+echo "fpm  failed ! ... "
