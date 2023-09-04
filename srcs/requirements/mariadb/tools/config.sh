cae559be9f51387b61cdab7f9ef942187c206012
spicyi <elkhayyate.driss@outlook.com>
2023-09-04 13:00:31 -0500

    maraidb need conf file

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -5,16 +5,16 @@ service mariadb start
 sleep 1
 
 mysql -sfu root  <<EOS
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'Elkhayyate@rootdb'; 
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; 
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-CREATE DATABASE wordpress;
-CREATE USER 'GAME_MASTER'@'localhost' IDENTIFIED BY 'TETRIS@MDB';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'GAME_MASTER'@'localhost';
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
 EOS
 
-mysqladmin -u root -p'Elkhayyate@rootdb' shutdown
+mysqladmin -u root -p'$MYSQL_ROOT_PASSWORD' shutdown
 mysqld --bind 0.0.0.0
