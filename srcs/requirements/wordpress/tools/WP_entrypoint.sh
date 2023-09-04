6ce7f36bc5150b4ff83e57cbfa24650da4be9c09
SpicyI <elkhayyate.driss@outlook.com>
2023-09-04 21:46:15 +0100

    php  listen  9000

--- srcs/requirements/wordpress/tools/WP_entrypoint.sh
+++ srcs/requirements/wordpress/tools/WP_entrypoint.sh
@@ -18,7 +18,8 @@ wp --allow-root core install --url="del-kahy.42.fr" --title="landing page" --adm
 
 
 # ! add user (default)
-
+sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#g' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's#chdir = /var/www#chdir = /var/www/wordpress#g' /etc/php/7.4/fpm/pool.d/www.conf
 echo "starting fpm ... "
 php-fpm7.4 -F
 echo "fpm  failed ! ... "
